d9b544d08e4c3de08d0566fddcaba55a8d95e113
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-17 15:37:07 +0530

    Stateless Functional Components.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,25 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-	render() {
-		const { avatar_url, html_url, login } = this.props.user;
-		return (
-			<div className='card text-center'>
-				<img
-					src={avatar_url}
-					alt=''
-					className='round-img'
-					style={{ width: "60px" }}
-				/>
-				<h3>{login}</h3>
-				<div>
-					<a className='btn btn-dark btn-sm my-1' href={html_url}>
-						More
-					</a>
-				</div>
+const UserItem = ({ user: { avatar_url, html_url, login } }) => {
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt=''
+				className='round-img'
+				style={{ width: "60px" }}
+			/>
+			<h3>{login}</h3>
+			<div>
+				<a className='btn btn-dark btn-sm my-1' href={html_url}>
+					More
+				</a>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
