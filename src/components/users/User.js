62941e23e0bf672b7efab22f85ea4b7dc0a7b731
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-18 15:10:26 +0530

    User component UI & Layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,104 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { Fragment, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-function User({ getUser, loading, user: { name } }) {
+function User({
+	getUser,
+	loading,
+	user: {
+		name,
+		avatar_url,
+		html_url,
+		hireable,
+		location,
+		bio,
+		company,
+		blog,
+		followers,
+		following,
+		public_repos,
+		public_gists,
+	},
+}) {
 	const { login } = useParams();
 
 	useEffect(() => {
 		getUser(login);
 	}, []);
+	if (loading) return <Spinner />;
 
-	return <div>{name}</div>;
+	return (
+		<Fragment>
+			<Link to='/' className='btn btn-light'>
+				Back to Search
+			</Link>
+			Hireable:{" "}
+			{hireable ? (
+				<i className='fas fa-check text-success' />
+			) : (
+				<i className='fas fa-times-circle text-danger' />
+			)}
+			<div className='card grid-2'>
+				<div className='all-center'>
+					<img
+						src={avatar_url}
+						alt=''
+						className='round-img'
+						style={{ width: "150px" }}
+					/>
+					<h1>{name}</h1>
+					<p>Location: {location}</p>
+				</div>
+				<div>
+					{bio && (
+						<Fragment>
+							<h3>Bio</h3>
+							<p>{bio}</p>
+						</Fragment>
+					)}
+					<a href={html_url} className='btn btn-dark my-1'>
+						Visit GitHub Profile
+					</a>
+					<ul>
+						<li>
+							{login && (
+								<Fragment>
+									<strong>Username: </strong> {login}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{company && (
+								<Fragment>
+									<strong>Company: </strong> {company}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{blog && (
+								<Fragment>
+									<strong>Website: </strong> {blog}
+								</Fragment>
+							)}
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className='card text-center'>
+				<div className='badge badge-primary'>Followers: {followers}</div>
+				<div className='badge badge-success'>Following: {following}</div>
+				<div className='badge badge-light'>Public Repos: {public_repos}</div>
+				<div className='badge badge-dark'>Public Gists: {public_gists}</div>
+			</div>
+		</Fragment>
+	);
 }
+
+User.propTypes = {
+	loading: PropTypes.bool,
+	user: PropTypes.object.isRequired,
+	getUser: PropTypes.func.isRequired,
+};
 
 export default User;
