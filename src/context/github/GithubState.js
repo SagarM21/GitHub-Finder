df827a03a0bb3317d19471bbb9841bc241474462
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-19 16:17:10 +0530

    Creating reducer and actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,15 +11,38 @@ import {
 	GET_REPOS,
 } from "../types";
 
-const GitHubState = (props) => {
+const GithubState = (props) => {
 	const initialState = {
 		users: [],
 		user: {},
 		repos: [],
 		loading: false,
+
 	};
 
-	const [dispatch, state] = useReducer(GithubReducer, initialState);
+	const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+	//search user
+	const searchUsers = async (text) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: SEARCH_USERS,
+			payload: res.data.items,
+		});
+	};
+
+	//get user
+
+	//get repos
+
+	//clear users
+
+	//set loading
+	const setLoading = () => dispatch({ type: SET_LOADING });
 
 	return (
 		<GithubContext.Provider
@@ -28,6 +51,7 @@ const GitHubState = (props) => {
 				user: state.user,
 				loading: state.loading,
 				repos: state.repos,
+                searchUsers
 			}}
 		>
 			{props.children}
@@ -35,4 +59,4 @@ const GitHubState = (props) => {
 	);
 };
 
-export default GitHubState;
+export default GithubState;
