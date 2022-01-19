a2d41c1569a383152c8f2f099a7c462d8eb4fc5d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-19 18:12:26 +0530

    Added notfound and home component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //switch is changed by routes
 import "./App.css";
 import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
-import Users from "./components/users/Users";
 import AlertState from "./context/alert/AlertState";
 import GitHubState from "./context/github/GithubState";
 
@@ -28,18 +28,10 @@ const App = () => {
 						<div className='container'>
 							<Alert />
 							<Routes>
-								<Route
-									exact
-									path='/'
-									element={
-										<Fragment>
-											<Search />
-											<Users />
-										</Fragment>
-									}
-								/>
+								<Route exact path='/' element={<Home />} />
 								<Route exact path='/about' element={<About />} />
 								<Route path={"/user/:login"} element={<User />} />
+								<Route path='*' element={<NotFound />} />
 							</Routes>
 						</div>
 					</div>
--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,11 +1,10 @@
 import React, { useReducer } from "react";
 import AlertContext from "./alertContext";
 import AlertReducer from "./alertReducer";
-
 import { SET_ALERT, REMOVE_ALERT } from "../types";
 
 const AlertState = (props) => {
-	const initialState = null ;
+	const initialState = null;
 
 	const [state, dispatch] = useReducer(AlertReducer, initialState);
 
