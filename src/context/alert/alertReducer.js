ec0c327238ec65f8dffd4adefd6fc3a11dec42b0
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-20 11:06:09 +0530

    Typerror: Cannot read properties of null in alertreducer fixed

--- src/context/alert/alertReducer.js
+++ src/context/alert/alertReducer.js
@@ -1,6 +1,6 @@
 import { SET_ALERT, REMOVE_ALERT } from "../types";
 
-export default (action, state) => {
+export default (state, action) => {
 	switch (action.type) {
 		case SET_ALERT:
 			return action.payload;
